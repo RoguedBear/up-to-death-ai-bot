97e451ebaf13c099b38a687a67e22de6344e6d33
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-08-12 14:07:53 +0530

    asyncio version - best_match

--- util/detect_game_scene.py
+++ util/detect_game_scene.py
@@ -1,9 +1,11 @@
+import asyncio
+import time
 from typing import Tuple
 
 import cv2.cv2 as cv2
+import imutils
 import mss
 import numpy as np
-import imutils
 
 
 def show_img(img: np.ndarray):
@@ -12,28 +14,38 @@ def show_img(img: np.ndarray):
     cv2.destroyAllWindows()
 
 
-def best_match(image: np.ndarray, template: np.ndarray, min_scale=0.2, max_scale=1) -> Tuple[int, Tuple[int, int], Tuple[int, int]]:
-    found = tuple()
+async def __best_match(image: np.ndarray, template: np.ndarray, tH: int, tW: int, scale: int) -> Tuple[
+        float, Tuple[int, int], float, float]:
+    resized = imutils.resize(image, width=int(image.shape[1] * scale))
+    ratio = image.shape[1] / float(resized.shape[1])
+
+    # image is smaller than template
+    if resized.shape[0] < tH or resized.shape[1] < tW:
+        return 0, (0, 0), 0, scale
+
+    # apply template matching
+    result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF_NORMED)
+    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+    return maxVal, maxLoc, ratio, scale
+
+
+def best_match(image: np.ndarray, template: np.ndarray, min_scale=0.2, max_scale=1) -> \
+        Tuple[int, Tuple[int, int], Tuple[int, int]]:
+    async def get_max(coros: list):
+        return max(await asyncio.gather(*coros), key=lambda x: x[0])
     (tH, tW) = template.shape[:2]
     result = None
     scale = 0
+
+    coroutines = []
     for scale in np.linspace(min_scale, 1, 20)[::-1]:
-        resized = imutils.resize(image, width=int(image.shape[1] * scale))
-        ratio = image.shape[1] / float(resized.shape[1])
-
-        # image is smaller than template
-        if resized.shape[0] < tH or resized.shape[1] < tW:
-            break
-
-        # apply template matching
-        result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF_NORMED)
-        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-
-        # check if threshold is bigger than prev stored one
-        if not found or maxVal > found[0]:
-            found = (maxVal, maxLoc, ratio)
-
-    (maxVal, maxLoc, ratio) = found
+        coroutines.append(__best_match(image, template, tH, tW, scale))
+    now = time.time()
+    coro_result = asyncio.run(get_max(coroutines))
+    print("Best match took: ", time.time() - now)
+    print(coro_result)
+    (maxVal, maxLoc, ratio, scale) = coro_result
     print(maxVal, maxLoc, ratio, scale)
     # show_img(result)
     (startX, startY) = (int(maxLoc[0] * ratio), int(maxLoc[1] * ratio))
