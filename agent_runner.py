a915d4c5d88c994b08b03a85ad99ea334db764bd
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-08-15 13:05:32 +0530

    add the file that runs the ai

---
+++ agent_runner.py
@@ -0,0 +1,86 @@
+import pathlib
+import time
+
+import keyboard
+import mss
+import numpy as np
+from cv2 import cv2
+from fastai.learner import load_learner
+
+from util.detect_game_scene import find_rough_coords
+from util.detect_game_start import is_spikes_visible, fine_tune
+from util.get_key import key_check
+
+# fix for fast ai
+# https://stackoverflow.com/a/64200241
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+def label_func(x): return x.parent.name
+
+print("Loading model...")
+model = load_learner("test2_12k_0.16.pkl")
+
+sct = mss.mss()
+template = cv2.imread("screens/splashscreen1.png")
+
+print("Finding game screen...")
+# monitor = {'top': 202, 'left': 6, 'width': 1221, 'height': 688}
+monitor = find_rough_coords(sct, template, 0.69)
+
+# fine tune
+while 0.5 > is_spikes_visible(np.array(sct.grab(monitor))):
+    pass
+monitor = fine_tune(np.array(sct.grab(monitor)), monitor)
+
+# wait to press space
+count = 0
+load = "-\\|/"
+while True:
+    print("Waiting for you to press ' ' space key... ", load[count], end="\r")
+    count += 1
+    if count == len(load):
+        count = 0
+    if key_check() == ' ':
+        print()
+        break
+    time.sleep(0.01)
+
+# the loop
+while True:
+    # ----
+    start = time.time()
+
+    # grab screenshot
+    image = np.array(sct.grab(monitor))
+    # ----
+
+    # convert to b/w
+    cv2.cvtColor(image, cv2.COLOR_BGR2GRAY, image)
+    # run canny detection
+    image = cv2.Canny(image, 157, 287)
+    # resize to 224x224
+    image = cv2.resize(image, (224, 224))
+
+    # show preview
+    cv2.imshow("preview", image)
+    cv2.waitKey(1)
+
+    # ask AI what to do
+    result, _, confidence = model.predict(image)
+
+    print(result.upper(), "confidence: ", [round(float(i), 2) for i in confidence])
+
+    if result == "left":
+        keyboard.press("a")
+        keyboard.release("d")
+    elif result == "right":
+        keyboard.press("d")
+        keyboard.release("a")
+    elif result == "nothing":
+        keyboard.release("a")
+        keyboard.release("d")
+
+    # ---
+    print("FPS: ", round(1/(time.time() - start), 0), end=" ")
